420ab4cd9592cc9dd7e2bab8b0f156ee0d7a7715
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-14 03:03:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,7 +6,7 @@ def unsafe?(speed)
  end
 end 
 def not_safe?(speed)
-if speed < 40 || speed > 60 ? return true : return false 
+ if speed < 40 || speed > 60 ? return true : return false 
  end
 end 
 	
